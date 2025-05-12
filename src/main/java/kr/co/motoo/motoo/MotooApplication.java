01a37c9f210e19127a2fc433393fdd2965bf5e34
kwonjh0406 <kwonjh0406@gmail.com>
2025-05-12 17:27:45 +0900

    init: 모투 스프링 프로젝트 생성

---
+++ src/main/java/kr/co/motoo/motoo/MotooApplication.java
@@ -0,0 +1,13 @@
+package kr.co.motoo.motoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotooApplication.class, args);
+	}
+
+}
