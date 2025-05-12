6f055ee196a6bfe66e35c53a9bfb200b3e4817c0
kwonjh0406 <kwonjh0406@gmail.com>
2025-05-12 18:00:00 +0900

    feat: lombok 의존성 추가 및 User 엔티티 초안

---
+++ src/main/java/kr/co/motoo/motoo/user/User.java
@@ -0,0 +1,26 @@
+package kr.co.motoo.motoo.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String nickname;
+
+    private LocalDateTime createdAt;
+}
